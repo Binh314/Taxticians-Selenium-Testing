33e5ad7a635bc6377c1c3ff2541cac0460727a94
Binh314 <binhphamc2002@gmail.com>
2024-08-24 18:07:48 -0400

    refactored by moving user portal navigation to utility class

--- src/test/java/com/skillstorm/cucumber/DashboardSteps.java
+++ src/test/java/com/skillstorm/cucumber/DashboardSteps.java
@@ -37,41 +37,7 @@ public class DashboardSteps {
         this.studioPage = new StudioPage(driver);
         dotenv = Dotenv.load();
 
-        driver.navigate().to(baseUrl);
-
-        WebElement usernameField = driver.findElement(By.id("txtUserID"));
-        usernameField.sendKeys(dotenv.get("USERNAME"));
-
-        WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        passwordField.sendKeys(dotenv.get("PASSWORD"));
-
-        WebElement loginBtn = driver.findElement(By.id("sub"));
-        loginBtn.click();
-
-        WebElement studioDropdown = driver.findElement(By.name("pzComposerBarMain_pyPortalHarness_2"));
-        studioDropdown.click();
-
-        Utility.sleep(100);
-
-        WebElement devStudioButton = driver.findElement(By.xpath("//*[text()='Dev Studio']"));
-        devStudioButton.click();
-
-        WebElement iframe = driver.findElement(By.id("Developer"));
-        driver.switchTo().frame(iframe);
-
-        WebElement launchPortalButton = driver.findElement(By.name("pzStudioHeader_pyDisplayHarness_7"));
-        launchPortalButton.click();
-
-        WebElement userPortalButton = driver.findElement(By.xpath("//*[text()='User Portal']"));
-        userPortalButton.click();
-
-        Utility.sleep(100);
-
-        Set<String> allTabs = driver.getWindowHandles();
-        ArrayList<String> tabs = new ArrayList<>(allTabs);
-
-        driver.switchTo().window(tabs.get(1));
-
+        Utility.goToUserPortal(driver);
     }
 
     
--- src/test/java/com/skillstorm/selenium/Utility.java
+++ src/test/java/com/skillstorm/selenium/Utility.java
@@ -1,10 +1,32 @@
 package com.skillstorm.selenium;
 
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.skillstorm.selenium.PegaLogin;
+import com.skillstorm.selenium.Utility;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+
+import com.skillstorm.selenium.StudioPage;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public final class Utility {
     private Utility() {
 
     }
-    
+
     /**
      * Puts the thread to sleep for a specified number of seconds.
      * 
@@ -17,4 +39,51 @@ public final class Utility {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Given a selenium web driver, uses it to navgiate to the User Portal in Pega.
+     * 
+     * @param driver Selenium web driver used to navigate to the user portal
+     */
+    public static void goToUserPortal(WebDriver driver) {
+        Dotenv dotenv = Dotenv.load();
+
+        String baseUrl = "http://10.1.0.5:9080/prweb/app/taxticians/";
+        driver.navigate().to(baseUrl);
+
+        WebElement usernameField = driver.findElement(By.id("txtUserID"));
+        usernameField.sendKeys(dotenv.get("USERNAME"));
+
+        WebElement passwordField = driver.findElement(By.id("txtPassword"));
+        passwordField.sendKeys(dotenv.get("PASSWORD"));
+
+        WebElement loginBtn = driver.findElement(By.id("sub"));
+        loginBtn.click();
+
+        WebElement studioDropdown = driver.findElement(By.name("pzComposerBarMain_pyPortalHarness_2"));
+        studioDropdown.click();
+
+        Utility.sleep(100);
+
+        WebElement devStudioButton = driver.findElement(By.xpath("//*[text()='Dev Studio']"));
+        devStudioButton.click();
+
+        WebElement iframe = driver.findElement(By.id("Developer"));
+        driver.switchTo().frame(iframe);
+
+        WebElement launchPortalButton = driver.findElement(By.name("pzStudioHeader_pyDisplayHarness_7"));
+        launchPortalButton.click();
+
+        WebElement userPortalButton = driver.findElement(By.xpath("//*[text()='User Portal']"));
+        userPortalButton.click();
+
+        Utility.sleep(100);
+
+        Set<String> allTabs = driver.getWindowHandles();
+        ArrayList<String> tabs = new ArrayList<>(allTabs);
+
+        driver.switchTo().window(tabs.get(1));
+    }
+    
+
 }
